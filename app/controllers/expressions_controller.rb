a8da5b3902af309672cb47010fdf7fd45061167a
unknown <paulclue20@gmail.com>
2021-09-29 15:15:28 -0500

    Initial commit

---
+++ app/controllers/expressions_controller.rb
@@ -0,0 +1,21 @@
+class ExpressionsController < ApplicationController
+  skip_before_action :verify_authenticity_token, raise: false
+  before_action :set_user, only: %i[show update]
+
+  def expression
+
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  private
+
+  def user_params
+    # params.require(:expression).permit(:username, :password, :email)
+    params.permit(:firstInd, :operation, :secondInt)
+  end
+end
