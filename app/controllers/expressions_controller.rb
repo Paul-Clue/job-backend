96d499a0a1b6d605d5417b51e50c06323139d9b4
unknown <paulclue20@gmail.com>
2021-09-29 17:32:21 -0500

    On the Wrong branch

--- app/controllers/expressions_controller.rb
+++ app/controllers/expressions_controller.rb
@@ -3,20 +3,48 @@ class ExpressionsController < ApplicationController
   before_action :set_user, only: %i[show update]
 
   def expression
-    @firstInt = params[:firstInt].to_i 
-    @secondInt = param[:secondInt].to_i 
+    @firstInt = params[:firstInt].to_i
+    @secondInt = param[:secondInt].to_i
 
     case params[:operation]
     when '+'
       @sum = @firsInt+@secondInt
+      @express = Expression.create(firstInt: params[:firstInt], operator: params[operator], secondInt: params[secondInt], result: @sum)
+      if @express.save
+        render json: { Expression: ExpressionSerializer.new(@express) }, status: :created
+      # render json: @user, status: :created
+      else
+        render json: { error: 'failed to calculate expression' }, status: :unauthorized
+      end
     when '-'
-      @difference = @firstInt-@secondInt
+      @difference = @firsInt-@secondInt
+      @express = Expression.create(firstInt: params[:firstInt], operator: params[operator], secondInt: params[secondInt], result: @difference)
+      if @express.save
+        render json: { Expression: ExpressionSerializer.new(@express) }, status: :created
+      # render json: @user, status: :created
+      else
+        render json: { error: 'failed to calculate expression' }, status: :unauthorized
+      end
     when '*'
       @product = @firsInt*@secondInt
+      @express = Expression.create(firstInt: params[:firstInt], operator: params[operator], secondInt: params[secondInt], result: @product)
+      if @express.save
+        render json: { Expression: ExpressionSerializer.new(@express) }, status: :created
+      # render json: @user, status: :created
+      else
+        render json: { error: 'failed to calculate expression' }, status: :unauthorized
+      end
     when '/'
       @quotient = @firsInt/@secondInt
+      @express = Expression.create(firstInt: params[:firstInt], operator: params[operator], secondInt: params[secondInt], result: @quotient)
+      if @express.save
+        render json: { Expression: ExpressionSerializer.new(@express) }, status: :created
+      # render json: @user, status: :created
+      else
+        render json: { error: 'failed to calculate expression' }, status: :unauthorized
+      end
     else
-      puts 'Only single-digit numbers are allowed'
+      render json: { error: 'failed to calculate expression' }, status: :unauthorized
     end
   end
 
@@ -29,7 +57,7 @@ class ExpressionsController < ApplicationController
   private
 
   def user_params
-    # params.require(:expression).permit(:username, :password, :email)
-    params.permit(:firstInd, :operation, :secondInt)
+    params.require(:expression).permit(:firstInd, :operation, :secondInt)
+    # params.permit(:firstInd, :operation, :secondInt)
   end
 end
